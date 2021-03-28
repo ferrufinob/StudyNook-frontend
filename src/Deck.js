3eeb185843cd26b316620355842b064c8b5c2fe6
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-28 17:07:40 -0400

    render click event Deck

--- src/Deck.js
+++ src/Deck.js
@@ -4,15 +4,31 @@ class Deck {
     this.id = id;
     this.name = name;
     this.cardCount = card_count;
+    this.deck = this.createDeck();
+    this.deck.addEventListener("click", this.handleClick);
+    // console.log(this);
     this.constructor.all.push(this);
   }
 
-  attachToDom() {
-    container.innerHTML += `
-        <div class="deck" data-id=${this.id}>
+  handleClick = (e) => {
+    console.log(e.target);
+    // when card gets clicked on render all its cards
+    // deck.renderCards(e.id)????
+  };
+
+  renderInnerHTML() {
+    return `
         <h3>${this.name}</h3>
         <p>${this.cardCount} terms</p>
-        </div>
         `;
+  }
+
+  createDeck() {
+    const deck = document.createElement("div");
+    deck.className = "deck";
+    deck.dataset.id = this.id;
+    deck.innerHTML = this.renderInnerHTML();
+    container.append(deck);
+    return deck;
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,34 +5,5 @@ const cardApi = new ApiService("http://localhost:3000/api/v1/decks");
 deckApi
   .getAllDecks()
   .then((decks) =>
-    decks.data.map((deck) =>
-      new Deck({ id: deck.id, ...deck.attributes }).attachToDom()
-    )
+    decks.data.map((deck) => new Deck({ id: deck.id, ...deck.attributes }))
   );
-
-function getCard(e) {
-  const deckId = e.target.dataset.id;
-  fetch(`http://localhost:3000/api/v1/decks/${deckId}/cards`)
-    .then((response) => response.json())
-    .then((cards) => {
-      cards.data.map((card) => {
-        console.log(card);
-        container.innerHTML += `
-        <div class="card">
-        <h2>Deck: ${card.attributes.deck_name}</h2>
-        <h3>${card.attributes.front}</h3>
-        <p>${card.attributes.back}</p>
-        </div>
-        `;
-      });
-    });
-}
-
-container.addEventListener("click", (e) => {
-  console.log("clicked");
-  // if (e.target.classList.contains("deck")) {
-  container.innerHTML = "";
-  getCard(e);
-  //get all cards belonging to this category
-  // }
-});
