f845261f75e0b1bd3a4799370fcd20a93f2ba1c2
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-28 01:47:43 -0400

    addd cardCount for decks

--- src/Deck.js
+++ src/Deck.js
@@ -1,14 +1,18 @@
 class Deck {
   static all = [];
-  constructor({ id, name }) {
+  constructor({ id, name, card_count }) {
     this.id = id;
     this.name = name;
+    this.cardCount = card_count;
     this.constructor.all.push(this);
   }
 
   attachToDom() {
     container.innerHTML += `
-        <div class="deck" data-id=${this.id}><h3>${this.name}</h3></div>
+        <div class="deck" data-id=${this.id}>
+        <h3>${this.name}</h3>
+        <p>${this.cardCount} terms</p>
+        </div>
         `;
   }
 }
