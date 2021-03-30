240d04a5d371265d39415b9efebbc6a49d04e9d2
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-29 23:58:57 -0400

    hide/show form

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ const deckContainer = document.querySelector("#deckContainer");
 const cardContainer = document.querySelector("#cardContainer");
 const deckApi = new ApiService("http://localhost:3000/api/v1");
 const cardApi = new ApiService("http://localhost:3000/api/v1/cards");
+const cardForm = document.querySelector(".form-container");
+const addCardBtn = document.querySelector(".add-card-btn");
+//⬇⬇  This is not good practice , refactor and move to form constructor
+let addCard = false;
 
 document.addEventListener("DOMContentLoaded", () => {
   deckApi.getAllDecks();
   cardApi.getAllCards();
+  addCardBtn.addEventListener("click", () => {
+    addCard = !addCard;
+    if (addCard) {
+      cardForm.style.display = "block";
+    } else {
+      cardForm.style.display = "none";
+    }
+  });
 });
 
 // goal: make a form constructor that will deal with the form later
